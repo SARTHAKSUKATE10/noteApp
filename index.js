748063ba8be92b1b335fb3583fe787f1a861b4b8
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-07-08 16:10:40 +0530

    File setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const path=require('path')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.set('view engine','ejs')
+
+app.get('/', function(req,res){
+    res.render("index")
+})
+
+app.listen(3000, function(){
+    console.log("its running")
+})
