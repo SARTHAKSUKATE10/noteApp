11c1cd8d57f668929388ad3003bf543654825963
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-07-09 11:23:34 +0530

    ui: created

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const path=require('path')
-
 const express = require('express')
 const app = express()
+const path=require('path')
+
+app.set("view engine","ejs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')))
 
-app.set('view engine','ejs')
-
 app.get('/', function(req,res){
     res.render("index")
 })
 
-app.listen(3000, function(){
-    console.log("its running")
-})
+app.listen(3000)
